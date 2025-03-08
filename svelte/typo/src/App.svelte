0bf056cfbaa443afa520ccc2c2b46c97940cbe68
Hugo Lynch <hugo@hopscotch.ca>
2025-03-08 13:58:50 -0500

    Add navigation bar

--- svelte/typo/src/App.svelte
+++ svelte/typo/src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import Board from './lib/Board.svelte'
+  import Nav from './lib/Nav.svelte'
 </script>
 
+<Nav />
 <main>
   <img id="logo" alt="Typo logo" src="/img/logo-typo.svg">
   <div class="board">
@@ -10,10 +12,13 @@
 </main>
 
 <style>
+  
   main {
     display: flex;
     flex-direction: column;
+    max-width: 1280px;
     gap: 32px;
+    align-items: center;
   }
 
   .board {
@@ -23,7 +28,7 @@
   }
 
   #logo {
-    max-width: 2r00px;
+    max-width: 200px;
     margin: 0 auto;
   }
 </style>
---
+++ svelte/typo/src/lib/Nav.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+
+</script>
+
+<nav>
+  <div class="row">
+    <a>LOGO</a>
+    <a>Trèfle</a>
+    <div class="spacer"></div>
+    <ul>User ⏷</ul>
+  </div>
+  <div class="row">
+    <a>←</a>
+    <div>Typo</div>
+    <ul>Game Mode ⏷</ul>
+  </div>
+  <div class="row">
+    <ul>Options ⏷</ul>
+    <div class="spacer"></div>
+    <button>?</button>
+    <button>Share</button>
+    <a>Leaderboards</a>
+  </div>
+</nav>
+
+<style>
+  nav {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    margin-bottom: 32px;
+    border-bottom: 1px solid var(--border-color-light);
+    padding: 16px;
+    background-color: var(--background-color-light);
+  }
+
+ .row {
+  display: flex;
+  gap: 8px;
+
+  &>* {
+    display: flex;
+    height: 32px;
+    border: 1px solid var(--border-color-light);
+    border-radius: var(--border-radius-small);
+    font-size: 1.6rem;
+    align-items: center;
+    padding: 0 8px;
+    background-color: white;
+  }
+
+  &>.spacer {
+    flex-grow: 1;
+    border: none;
+    background-color: transparent;
+  }
+ }
+</style>
